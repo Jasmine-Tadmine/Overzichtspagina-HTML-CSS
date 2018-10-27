4e2c5b05953162f452f377d49510ca1ed8ddad09
Jasmine-Tadmine <43673393+Jasmine-Tadmine@users.noreply.github.com>
2018-10-28 01:36:57 +0200

    Add files via upload

---
+++ Js/verhaal.js
@@ -0,0 +1,25 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+
+var verhaal = document.getElementById('story');
+
+verhaal.onclick = function () {
+    if (verhaal.src.includes('Images/Alzheimer-03.png')) {
+        verhaal.src = '../Images/Alzheimer-p2.gif';
+    } else
+    if (verhaal.src.includes('Images/Alzheimer-p2.gif')) {
+        verhaal.src = '../Images/Alzheimer-p3.gif';
+    } else
+    if (verhaal.src.includes('Images/Alzheimer-p3.gif')) {
+        verhaal.src = '../Images/Alzheimer-p4.gif';
+    } else
+    if (verhaal.src.includes('Images/Alzheimer-p4.gif')) {
+        verhaal.src = '../Images/Alzheimer-p1.gif'
+    } else
+    if (verhaal.src.includes('Images/Alzheimer-p1.gif')) {
+        verhaal.src = '../Images/Alzheimer-p2.gif'
+    }
+};
