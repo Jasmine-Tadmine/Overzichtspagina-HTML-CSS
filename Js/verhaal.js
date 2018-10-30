e06ccb00af18f0675f5dd29d8c2e68c3774a5858
Jasmine-Tadmine <43673393+Jasmine-Tadmine@users.noreply.github.com>
2018-10-30 11:12:36 +0100

    Add files via upload

--- Js/verhaal.js
+++ Js/verhaal.js
@@ -4,7 +4,7 @@
 
 /*eslint 'no-console': 0*/
 
-var verhaal = document.getElementById('story');
+var verhaal = document.getElementById('stories');
 
 verhaal.onclick = function () {
     if (verhaal.src.includes('Images/Alzheimer-03.png')) {
@@ -17,9 +17,32 @@ verhaal.onclick = function () {
         verhaal.src = '../Images/Alzheimer-p4.gif';
     } else
     if (verhaal.src.includes('Images/Alzheimer-p4.gif')) {
-        verhaal.src = '../Images/Alzheimer-p1.gif'
+        verhaal.src = '../Images/Alzheimer-p1.gif';
     } else
     if (verhaal.src.includes('Images/Alzheimer-p1.gif')) {
-        verhaal.src = '../Images/Alzheimer-p2.gif'
+        verhaal.src = '../Images/Alzheimer-p2.gif';
+    }
+};
+
+var mobiel = document.getElementById('mobiel');
+
+ mobiel.onclick = function () {
+    if (mobiel.src.includes('Images/Alzheimer-mobiel.png')) {
+        mobiel.src = '../Images/Alzheimer-mobiel-p1.gif';
+    } else
+    if (mobiel.src.includes('Images/Alzheimer-mobiel-p1.gif')) {
+        mobiel.src = '../Images/Alzheimer-mobiel-p2.gif';
+    } else
+    if (mobiel.src.includes('Images/Alzheimer-mobiel-p2.gif')) {
+        mobiel.src = '../Images/Alzheimer-mobiel-p3.gif';
+    } else
+    if (mobiel.src.includes('Images/Alzheimer-mobiel-p3.gif')) {
+        mobiel.src = '../Images/Alzheimer-mobiel-p4.gif';
+    } else
+    if (mobiel.src.includes('Images/Alzheimer-mobiel-p4.gif')) {
+        mobiel.src = '../Images/Alzheimer-mobiel-p5.gif';
+    } else
+    if (mobiel.src.includes('Images/Alzheimer-mobiel-p5.gif')) {
+        mobiel.src = '../Images/Alzheimer-mobiel-p1.gif';
     }
 };
